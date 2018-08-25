019cb26b70d72706fdc9430c6dc91702d0a4dddc
Pan Cheng <pchengeducation@hotmail.com>
2018-08-25 16:10:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    redirect '/posts'
   end
+
+  get 'posts/new do'
+    erb :'new.html'
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    erb :'show.html'
+  end
+
+  get '/post' do
+    @posts = Post.all
+    erb :'index.html'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show.html'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :'edit.html'
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(param[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :'show.html'
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :'delete.html'
+  end
+
 end
